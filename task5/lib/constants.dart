33f49e875bec18ed9c38b971325e2003c8e4f9d5
Abdou kdj <abdoukadjoudj@gmail.com>
2026-01-23 21:24:22 +0100

    this constant file will contain all colors, fonts... that i will use several times

---
+++ task5/lib/constants.dart
@@ -0,0 +1,12 @@
+// COLORS OF Onboarding screens
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFF1F41BB);
+const lightblue = Color.fromARGB(255, 224, 227, 250);
+const white = Colors.white;
+const black = Colors.black;
+
+//
+const h1 = TextStyle(fontSize: 30, fontWeight: FontWeight.w500, color: primary);
+const h2 = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+const body = TextStyle(fontSize: 14);
