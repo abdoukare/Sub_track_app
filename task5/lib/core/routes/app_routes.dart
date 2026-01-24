315661eeff2fa3b2eaa70b8bf9f7ffeef260be96
Abdou kdj <abdoukadjoudj@gmail.com>
2026-01-24 22:39:23 +0100

    feat: define authentication navigation routes

--- task5/lib/core/routes/app_routes.dart
+++ task5/lib/core/routes/app_routes.dart
@@ -1,1 +1,6 @@
-
+class AppRoutes {
+  // Authentication routes
+  static const String signUp = "/sign-up";
+  static const String logIn = "/sign-in";
+  static const String signOut = "/sign-out";
+}
