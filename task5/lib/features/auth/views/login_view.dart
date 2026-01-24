7ad2ff8dff6a13404ca55f5393e7ef3e3d78cda0
Abdou kdj <abdoukadjoudj@gmail.com>
2026-01-24 22:41:16 +0100

    feat: add login screen UI with form inputs
    - Create LoginView with email and password input fields

--- task5/lib/features/auth/views/login_view.dart
+++ task5/lib/features/auth/views/login_view.dart
@@ -1,1 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:task5/core/constants/constants.dart';
 
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: -410,
+            right: -410,
+            child: Container(
+              height: 630,
+              width: 630,
+              decoration: BoxDecoration(
+                color: lightblue,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Text("Login here ", style: h2),
+                Text(
+                  "Welcome back to you app!!",
+                  style: h2.copyWith(fontSize: 18, color: black),
+                ),
+                SizedBox(height: 100),
+                TextField(
+                  //maxLength: 100,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: primary, width: 2.0),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: primary, width: 2.0),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "password",
+                    focusedBorder: OutlineInputBorder(
+                      //borderSide: BorderSide(color: primary, width: 2.0),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      // borderSide: BorderSide(color: primary, width: 2.0),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "forgot password?",
+                      style: TextStyle(
+                        color: primary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primary,
+                      foregroundColor: white,
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Sign in",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text("Create a new account"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
