11687e3e95d1cc31e9a6f6dba5dd7713e7a41fd5
Abdou kdj <abdoukadjoudj@gmail.com>
2026-02-09 20:49:03 +0100

    add: routes of the pages because i changed the strategy, i did not find a solution yet for the backend respondign, so i did fake api to get the ui done

--- task5/lib/core/routes/app_pages.dart
+++ task5/lib/core/routes/app_pages.dart
@@ -1,1 +1,13 @@
+import 'package:get/get.dart';
+import 'package:task5/features/auth/views/OnboardingScreen.dart';
+import 'package:task5/features/subscriptions/viewmodels/home.dart';
+import 'package:task5/features/auth/views/login_view.dart';
 
+class AppPages {
+  static final routes = [
+    GetPage(name: '/sign-in', page: () => LoginView()),
+    GetPage(name: '/sign-out', page: () => LoginView()),
+    GetPage(name: '/home', page: () => HomeView()),
+    GetPage(name: '/onboarding', page: () => Onboardingscreen()),
+  ];
+}
--- task5/lib/core/routes/app_routes.dart
+++ task5/lib/core/routes/app_routes.dart
@@ -3,4 +3,10 @@ class AppRoutes {
   static const String signUp = "/sign-up";
   static const String logIn = "/sign-in";
   static const String signOut = "/sign-out";
+  // Home route
+  static const String home = "/home";
+
+  static const String onboarding = "/onboarding";
+
+
 }
